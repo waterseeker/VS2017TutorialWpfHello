2676805c1b1d7bd232dd95cdd655b8777950b80a
Burris, Wayne <Wayne.Burris@protective.com>
2017-09-01 08:29:51 -0500

    first commit

--- WpfHello/MainWindow.xaml.cs
+++ WpfHello/MainWindow.xaml.cs
@@ -24,5 +24,10 @@ namespace WpfHello
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Hello!");
+        }
     }
 }
